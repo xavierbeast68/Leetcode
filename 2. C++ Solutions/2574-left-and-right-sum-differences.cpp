d3bb6cc64a5382c4611da686492cae5e8101ac7d
xavierbeast68 <sdby101@gmail.com>
2023-02-26 21:01:32 +0530

    26-02-2023

---
+++ 2. C++ Solutions/2574-left-and-right-sum-differences.cpp
@@ -0,0 +1,43 @@
+//* https://leetcode.com/problems/left-and-right-sum-differences/description/
+//* https://leetcode.com/problems/left-and-right-sum-differences/submissions/905208967/
+
+class Solution {
+public:
+    void prefix(vector<int>& pre, vector<int>& nums, int n){
+        pre[0] = nums[0];
+        for(int i = 1 ; i < n ; ++i){
+            pre[i] = pre[i-1] + nums[i];
+        }
+    }
+    void suffix(vector<int>& suff, vector<int>& nums, int n){
+        suff[n-1] = nums[n-1];
+        for(int i = n-2 ; i >= 0 ; --i){
+            suff[i] = suff[i+1] + nums[i];
+        }
+    }
+    vector<int> leftRigthDifference(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> answer(n,0);
+        if(n==1){
+            return answer;
+        }
+        
+        vector<int> pre(n,0);
+        vector<int> suff(n,0);
+        prefix(pre, nums, n);
+        suffix(suff, nums, n);
+        
+        for(int i = 0 ; i < n ; ++i){
+            if(i==0){
+                answer[i] = suff[i+1];
+            }
+            else if(i == n-1){
+                answer[i] = pre[i-1];
+            }
+            else{
+                answer[i] = abs(pre[i-1] - suff[i+1]);
+            }
+        }
+        return answer;
+    }
+};
