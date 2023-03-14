cad2240555f2abebddfe32e51dd8860092c82cf7
xavierbeast68 <sdby101@gmail.com>
2023-03-15 00:12:03 +0530

    14-03-2023

---
+++ 2. Solved Problems/387. First Unique Character in a String/387-first-unique-character-in-a-string.cpp
@@ -0,0 +1,27 @@
+//* https://leetcode.com/problems/first-unique-character-in-a-string/description/
+
+
+class Solution {
+public:
+    int firstUniqChar(string s) {
+        queue<char> q;
+        int freq[26] = {0};
+        for(int i = 0; i < s.length(); ++i){
+            char ch = s[i];
+            q.push(ch);
+            freq[ch - 'a']++;
+
+            while(!q.empty() && freq[q.front() - 'a'] > 1){
+                q.pop();
+            }
+        }
+
+        if(q.empty()){
+            return -1;
+        }
+
+        else{
+            return s.find(q.front());
+        }
+    }
+};
