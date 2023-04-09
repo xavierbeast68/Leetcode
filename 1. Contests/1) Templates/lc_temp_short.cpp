65b162c20f35fd71e4e0452a04594460ebd99d72
xavierbeast68 <sdby101@gmail.com>
2023-04-09 14:09:11 +0530

    update

--- 1. Contests/1) Templates/lc_temp_short.cpp
+++ 1. Contests/1) Templates/lc_temp_short.cpp
@@ -13,8 +13,6 @@
  * AVOIDING COMPLEXITY, REDUCES BUGS.
  */
 
-#pragma GCC optimize("O3,Ofast,unroll-loops")
-
 #include <bits/stdc++.h>
 
 using namespace std;
@@ -24,6 +22,7 @@ using namespace chrono;
 using ll                                            = long long;
 using lld                                           = long double;
 #define vi                                          vector<int>
+#define vvi                                         vector<vi>
 
 constexpr lld EPS                                   = 1e-9;
 constexpr ll MOD                                    = 1e9 + 7; //1000000007
@@ -49,8 +48,14 @@ constexpr ll mod1                                   = 998244353;
 #define println(x)                                  cout<<(x)<<endl
 
 // Vector Read & Print ->
-template <class T> vector<T> readvector(T n) { vector<T> arr(n); for (int i = 0; i < n; i++) {cin >> arr[i];} return arr;}   // vector<ll> arr = readvector(n);
-template <class T> void printvector(vector<T> arr, T n) {for (int i = 0; i < n; i++) {cout << arr[i] << " ";} cout << endl;}   // printvector(arr, n);
+// template <class T> vector<T> readvector(T n) { vector<T> arr(n); for (int i = 0; i < n; i++) {cin >> arr[i];} return arr;}   // vector<ll> arr = readvector(n);
+// template <class T> void printvector(vector<T> arr) {for (int i = 0; i < (int)arr.size(); i++) {cout << arr[i] << " ";} cout << endl;}   // printvector(arr, n);
+
+// Streams ->
+template<class T, class V>istream& operator>>(istream &in, pair<T, V> &a){in >> a.F >> a.S;return in;}
+template<class T>istream& operator>>(istream &in, vector<T> &a){for(auto &i: a){in >> i;} return in;}
+template<class T, class V>ostream& operator<<(ostream &os, pair<T, V> &a){os << a.F << " " << a.S;return os;}
+template<class T>ostream& operator<<(ostream &os, vector<T> &a){for(int i = 0 ; i < sz(a) ; i++){if(i != 0){os << ' ';}os << a[i];}return os;}
 
 
 /*--------------------------------------------------------------Debugger--------------------------------------------------------------*/
@@ -59,32 +64,6 @@ template <class T> void printvector(vector<T> arr, T n) {for (int i = 0; i < n; 
 //  where you are also the murderer.
 
 #define test(i)                                     cout << "(#" << i << ")" << endl
-
-// #ifndef ONLINE_JUDGE
-#ifdef XAVIERBEAST
-#define dbg(x)                                      cerr << #x<<" "; _print(x); cerr << endl;
-#else
-#define dbg(x);
-#endif
-
-void _print(int t) {cerr << t;}
-void _print(ll t) {cerr << t;}
-void _print(string t) {cerr << t;}
-void _print(char t) {cerr << t;}
-void _print(double t) {cerr << t;}
-
-template <class T, class V> void _print(pair <T, V> p);
-template <class T> void _print(vector <T> v);
-template <class T> void _print(set <T> v);
-template <class T, class V> void _print(map <T, V> v);
-template <class T> void _print(multiset <T> v);
-template <class T, class V> void _print(pair <T, V> p) {cerr << "{"; _print(p.ff); cerr << ","; _print(p.ss); cerr << "}";}
-template <class T> void _print(vector <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(set <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T> void _print(multiset <T> v) {cerr << "[ "; for (T i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-template <class T, class V> void _print(unordered_map <T, V> v) {cerr << "[ "; for (auto i : v) {_print(i); cerr << " ";} cerr << "]";}
-
 
 /*------------------------------------------------------Functions------------------------------------------------------*/
 
@@ -95,7 +74,6 @@ ll log_a_to_base_b(ll a, ll b)                      {return log2(a) / log2(b);}
 ll isPowerof2(ll x)                                 {return (x && !(x & (x - 1)));} // Checking if given 64 bit integer is power of 2
 bool is_whole(ll a)                                 {return (a - floor(a) < 1e-9);} // floor(a)==ceil(a)
 ll factorial(const int& p)                          {if (p <= 1) {return 1;} return p * factorial(p - 1);}
-bool isPrime(ll n)                                  {if (n == 2 || n == 3) {return true;} if (n <= 1 || n % 2 == 0 || n % 3 == 0) {return false;} for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) {return false;}} return true;}
 double nCr(ll n, ll r)                                  {double sum = 1; for(int i = 1; i <= r; ++i){sum = sum * (n - r + i) / i;} return sum;}
 ll binpow(ll a , ll b)                              {if (b == 0) {return 1;} if (b == 1) {return a;} if (b % 2 == 0) {return binpow((a * a) % MOD, b / 2);} else {return (a * binpow((a * a) % MOD, b / 2)) % MOD;}}    // binary exponentiation
 
@@ -104,9 +82,29 @@ ll mod_add(ll a, ll b)                              {a = a % MOD; b = b % MOD; r
 ll mod_mul(ll a, ll b)                              {a = a % MOD; b = b % MOD; return (mod(a*b)); }
 ll mod_sub(ll a, ll b)                              {a = a % MOD; b = b % MOD; return (mod(a-b));}
 
+
 // random number generator
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 ll getRandomNumber(ll l, ll r)                      {return uniform_int_distribution<ll>(l, r)(rng); }
+
+// Prime utility
+bool isPrime(ll n)                                  {if (n == 2 || n == 3) {return true;} if (n <= 1 || n % 2 == 0 || n % 3 == 0) {return false;} for (int i = 5; i * i <= n; i += 6) { if (n % i == 0 || n % (i + 2) == 0) {return false;}} return true;}
+
+int sz = 1e6 + 5;
+bool PrimeSieve[1000005]; // 1e6+5
+void buildSieve(){
+    for (int i = 2; i <= sz; i++)
+        PrimeSieve[i] = 1;
+    PrimeSieve[0] = 0; //
+    PrimeSieve[1] = 0; // 1 is neither prime nor composite
+    for (int i = 2; i < sz; i++){
+        if (PrimeSieve[i] == 0)
+            continue; // the current number itself is composite
+        for (int j = 2; j * i < sz; j++){
+            PrimeSieve[i * j] = 0;
+        }
+    }
+}
 
 
 /*-------------------------------------------------------||||||||||-----------------------------------------------------------------------
@@ -120,54 +118,30 @@ ll getRandomNumber(ll l, ll r)                      {return uniform_int_distribu
 
 class Solution {
 public:
-    
+    // function->
 };
 
-
-/*---------------------------------------- DON'T TOUCH IT ----------------------------------------*/
-
 /*
-void solve()
-{
+void solve(){
     // Let's Code
-
+    // read parameters
+    Solution obj;
+    auto ans //= obj.function_name(parameters) ;
+    cout << ans << endl;
 }
 
-#define fastio                                      ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+//---------------------------------------- DON'T TOUCH IT ----------------------------------------
 
-//InputOutputError_From/To_File ->
-void file_io()
-{
-// #ifndef ONLINE_JUDGE
-#ifdef XAVIERBEAST
-    freopen("input.txt", "r", stdin);
-    freopen("output.txt", "w", stdout);
-    freopen("error.txt", "w", stderr);
-#endif
-}
-
-signed main()
-{
-    fastio;
+signed main(){
 
     cout << fixed << setprecision(10);
-    // file_io();
-    auto start = high_resolution_clock::now();
 
     //testcases=1: default value for single test case
     int testcases = 1;
     //cin >> testcases;
-    while (testcases--)
-    {
+    while (testcases--){
         solve();
     }
-
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<microseconds>(stop - start);
-    // #ifndef ONLINE_JUDGE
-    #ifdef XAVIERBEAST
-        cerr << endl << "Time: " << (float)duration.count()/1000000 << " s" << endl;
-    #endif
 
     return 0;
 }
